964c68cf52ee2c49f14ca2c89f21b6f9e9bd9f82
GuglielmoBini <guglielmobini@gmail.com>
2023-02-24 18:13:45 +0100

    corrections

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,14 @@
 @extends('layouts.main')
 @section('title', 'home')
 @section('content')
-<main>
-    {{-- main content --}}
-    <section id="movies">
-        <div class="container">
-            <h3 class="mt-5">Your Movies</h3>
-            <div class="card-container d-flex flex-wrap justify-content-center">
-              @foreach ($movies as $movie)                  
-                @include('movies.card')
-              @endforeach
-            </div>
-        </div>
-    </section>
-</main>
+  <section id="movies">
+      <div class="container">
+          <h3 class="mt-5">Your Movies</h3>
+          <div class="card-container d-flex flex-wrap justify-content-center">
+            @foreach ($movies as $movie)                  
+              @include('movies.card')
+            @endforeach
+          </div>
+      </div>
+  </section>
 @endsection
